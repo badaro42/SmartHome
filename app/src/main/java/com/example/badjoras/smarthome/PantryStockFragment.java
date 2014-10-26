0aa959154b262e31bb58046dfc5aa234a3ec4497
Rafael Bizarra <r.bizarra@campus.fct.unl.pt>
2014-10-26 17:34:30 +0000

    [Cozinha] Tabela com o stock implementada
    -Falta adicionar o comportamento à tabela: adicionar/remover stock,
    adicionar novo produto.
    -Falta um spinner (ou radio button???) para ordenar a tabela por
    quantidade de stock ou ordem alfabetica

--- app/src/main/java/com/example/badjoras/smarthome/PantryStockFragment.java
+++ app/src/main/java/com/example/badjoras/smarthome/PantryStockFragment.java
@@ -1,17 +1,25 @@
 package com.example.badjoras.smarthome;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.example.badjoras.smarthome.ListViewAdapter.FIRST_COLUMN;
+import static com.example.badjoras.smarthome.ListViewAdapter.SECOND_COLUMN;
+//import static com.example.badjoras.smarthome.ListViewAdapter.THIRD_COLUMN;
+//import static com.example.badjoras.smarthome.ListViewAdapter.FOURTH_COLUMN;
 
 
 public class PantryStockFragment extends Fragment {
@@ -28,6 +36,9 @@ public class PantryStockFragment extends Fragment {
     private Button button;
     private String messsage;
 
+    private ArrayList<HashMap<String, String>> list;
+    private Bundle bundle;
+
     public static PantryStockFragment newInstance(int position, String function) {
         PantryStockFragment f = new PantryStockFragment();
         Bundle b = new Bundle();
@@ -40,6 +51,7 @@ public class PantryStockFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bundle = savedInstanceState;
 
         position = getArguments().getInt(ARG_POSITION);
         function = getArguments().getString(ARG_FUNCTION);
@@ -52,36 +64,117 @@ public class PantryStockFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.pantry_stock_fragment, container,
                 false);
 
-        textField = (EditText) rootView.findViewById(R.id.editText_pantryStock); //reference to the text field
-        button = (Button) rootView.findViewById(R.id.button_pantryStock);   //reference to the send button
+        ListView listView = (ListView) rootView.findViewById(R.id.listView1);
 
-        //Button press event listener
-        button.setOnClickListener(new View.OnClickListener() {
+        populateList();
 
-            public void onClick(View v) {
+        ListViewAdapter adapter = new ListViewAdapter(this, list, bundle);
+        listView.setAdapter(adapter);
 
-                messsage = textField.getText().toString(); //get the text message on the text field
-                textField.setText("");      //Reset the text field to blank
+        listView.setOnTouchListener(new View.OnTouchListener() {
 
-                try {
-//                    client = new Socket("192.168.2.2", 4444);  //ip da rede eduroam
-                    client = new Socket("192.168.1.78", 4444);  //ip de casa
-//                    client = new Socket("10.171.240.101", 4444);  //este ip é do hotspot BALELE
-                    printwriter = new PrintWriter(client.getOutputStream(),true);
-                    printwriter.write(messsage);  //write the message to output stream
-
-                    printwriter.flush();
-                    printwriter.close();
-                    client.close();   //closing the connection
-
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
             }
         });
 
+
+
+//        textField = (EditText) rootView.findViewById(R.id.editText_pantryStock); //reference to the text field
+//        button = (Button) rootView.findViewById(R.id.button_pantryStock);   //reference to the send button
+//
+//        //Button press event listener
+//        button.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View v) {
+//
+//                messsage = textField.getText().toString(); //get the text message on the text field
+//                textField.setText("");      //Reset the text field to blank
+//
+//                try {
+////                    client = new Socket("192.168.2.2", 4444);  //ip da rede eduroam
+//                    client = new Socket("192.168.1.78", 4444);  //ip de casa
+////                    client = new Socket("10.171.240.101", 4444);  //este ip é do hotspot BALELE
+//                    printwriter = new PrintWriter(client.getOutputStream(),true);
+//                    printwriter.write(messsage);  //write the message to output stream
+//
+//                    printwriter.flush();
+//                    printwriter.close();
+//                    client.close();   //closing the connection
+//
+//                } catch (UnknownHostException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+
         return rootView;
+    }
+
+
+    private void populateList() {
+        // TODO Auto-generated method stub
+
+        list=new ArrayList<HashMap<String,String>>();
+        HashMap<String,String> temp;
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Tomate");
+        temp.put(SECOND_COLUMN, "5");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Alface");
+        temp.put(SECOND_COLUMN, "2");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Lata de Atum");
+        temp.put(SECOND_COLUMN, "0");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Esparguete");
+        temp.put(SECOND_COLUMN, "3");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Cebola");
+        temp.put(SECOND_COLUMN, "7");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Batata");
+        temp.put(SECOND_COLUMN, "25");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Alho francês");
+        temp.put(SECOND_COLUMN, "1");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Lata de Salsichas");
+        temp.put(SECOND_COLUMN, "2");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Arroz Agulha");
+        temp.put(SECOND_COLUMN, "1");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Café");
+        temp.put(SECOND_COLUMN, "2");
+        list.add(temp);
+
+        temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Papel Higiénico");
+        temp.put(SECOND_COLUMN, "30");
+        list.add(temp);
     }
 }
