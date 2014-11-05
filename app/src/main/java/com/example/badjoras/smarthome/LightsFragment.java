4e7dceed199fa1dcdb9a70d254efbb9eb68fa36e
DiogoCanteiro <diogocanteiro@gmail.com>
2014-11-05 16:22:53 +0000

    Lights
    Alterei o fragment e o layout. Depois é preciso tirar o texto estático
    que tá na variável

--- app/src/main/java/com/example/badjoras/smarthome/LightsFragment.java
+++ app/src/main/java/com/example/badjoras/smarthome/LightsFragment.java
@@ -8,13 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.badjoras.control.Home;
 import com.example.badjoras.control.Light;
 import com.example.badjoras.control.Room;
 
-import static com.example.badjoras.smarthome.MainActivity.*;
-import static com.example.badjoras.control.Light.*;
+import static com.example.badjoras.smarthome.MainActivity.LIGHTS;
 
 /**
  * Created by Rafael on 16/10/2014.
@@ -31,6 +31,7 @@ public class LightsFragment extends Fragment {
 
     private SeekBar sb;
     private ImageView image;
+    private TextView text;
 
     public static LightsFragment newInstance(int position, String function, String title) {
         LightsFragment f = new LightsFragment();
@@ -58,6 +59,9 @@ public class LightsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.lights_fragment, container, false);
         sb = (SeekBar) rootView.findViewById(R.id.seekBarLights);
         image = (ImageView) rootView.findViewById(R.id.imageLampView);
+        text = (TextView) rootView.findViewById(R.id.luminosityTextView);
+        //text.setText("Luminosidade" + getLuminosidade() + "%");
+        text.setText("Luminosidade 0%");
 
 
         sb.setOnTouchListener(new View.OnTouchListener() {
@@ -72,6 +76,8 @@ public class LightsFragment extends Fragment {
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+               text.setText("Luminosidade " + progress + "%");
                if(progress==0)
                    image.setImageResource(R.drawable.off_lamp_icon);
                 else if(progress>0 && progress<=50)
