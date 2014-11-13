918de1a8b19486250611ecc3dc2b0d1de77334f9
Rafael Bizarra <r.bizarra@campus.fct.unl.pt>
2014-11-13 10:37:41 +0000

    Actualizada criação das classes para cada Room

--- app/src/main/java/com/example/badjoras/control/Room.java
+++ app/src/main/java/com/example/badjoras/control/Room.java
@@ -25,19 +25,19 @@ public class Room implements Serializable {
             features.put(PANTRY_STOCK, new PantryStock());
             features.put(AIR_CONDITIONER, new AirConditioner());
             features.put(LIGHTS, new Light());
-            features.put(BLINDS, new Feature());
+            features.put(BLINDS, new Blinds());
             features.put(COFFEE_MACHINE, new Feature());
             features.put(STOVE_OVEN, new Feature());
         }
         else if (room.equalsIgnoreCase(BEDROOM)) {
             features.put(AIR_CONDITIONER, new AirConditioner());
             features.put(LIGHTS, new Light());
-            features.put(BLINDS, new Feature());
+            features.put(BLINDS, new Blinds());
         }
         else if (room.equalsIgnoreCase(LIVING_ROOM)) {
             features.put(AIR_CONDITIONER, new AirConditioner());
             features.put(LIGHTS, new Light());
-            features.put(BLINDS, new Feature());
+            features.put(BLINDS, new Blinds());
         }
         else if (room.equalsIgnoreCase(OUTSIDE_GENERAL)) {
             features.put(SPRINKLER, new Feature());
